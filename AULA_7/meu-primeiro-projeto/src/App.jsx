c8b1d441f126bf9b5eca9e17a3d6d328526b282d
Douglas <douglasnogueirapjunior@gmail.com>
2026-03-25 16:21:52 -0300

    .

---
+++ AULA_7/meu-primeiro-projeto/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+
+    <div>
+      <h1>Olá, React!</h1>
+      <p>Estou alterando meu primeiro componente.</p>
+
+      <div style={{ padding: '20px' }}>
+        <h1>Minha primeira aula de react</h1>
+
+        {/* Aqui chamamos o componente que criamos acima */}
+        <Saudacao />
+        <Saudacao />
+        <Saudacao />
+
+        <p>note que posso repetir o componente quantas vezes quiser</p>
+        <hr />
+      </div>
+
+      <div>
+        <h1>Intervalo</h1>
+
+        {/* Aqui chamamos o componente que criamos acima */}
+        <Intervalo />
+        <hr />
+      </div>
+
+      <div>
+        <h1>Intervalo</h1>
+
+        {/* Aqui chamamos o componente que criamos acima */}
+        <Alarme />
+      </div>
+
+    </div>
+    
+  )
+}
+
+function Saudacao() {
+  return (
+    <div style={{ backgroundColor: '#f0f0f0', padding: '10px', borderRadius: '8px', marginBottom: '10px' }}>
+      <h2 style={{ color: '#007bff' }}>Olá Alunos!</h2>
+      <p>este componente foi criado separadamente</p>
+    </div>
+
+  );
+}
+
+function Intervalo() {
+  return (
+    <div style={{ backgroundColor: '#af01ff', padding: '10px', borderRadius: '8px', marginBottom: '10px' }}>
+      <h2 style={{ color: '#007bff' }}>É hora do intervalo!</h2>
+      <p>este componente foi criado separadamente</p>
+    </div>
+
+  );
+}
+
+function Alarme() {
+  return (
+    <div style={{ backgroundColor: '#01f2fa', padding: '10px', borderRadius: '8px', marginBottom: '10px' }}>
+      <h2 style={{ color: '#007bff' }}>O alarme esra tocando!</h2>
+      <p>este componente foi criado separadamente</p>
+    </div>
+
+  );
+}
+
+export default App
